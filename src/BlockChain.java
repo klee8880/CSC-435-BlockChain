d452524baa8d6ed4f7c1290b66a250514619933e
klee8880 <klee8880@gmail.com>
2020-05-11 23:50:14 -0500

    Reorganized project / parse arguments

--- src/BlockChain.java
+++ src/BlockChain.java
@@ -1,16 +1,81 @@
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+public class BlockChain {
+	
+	private static String hashType = "SHA-256";
+	
+	static MessageDigest encoder;
+
+	public static void main (String [] args) {
+
+		//Start up encoder
+		try {
+			encoder = MessageDigest.getInstance(hashType);
+		} 
+		//If encoding variable fails immediately close program after printing error code.
+		catch (NoSuchAlgorithmException exp) {
+			exp.printStackTrace();
+			return;
+		}
+		
+		//Run the process
+		new BlockChain().run(args);
+
+	}
+	
+	void run(String [] args) {
+		
+		//TODO: Instantiate variables
+		int port = 0;
+		int unverifiedPort = 4710;
+		int blockChainPort = 4810;
+		Boolean solved = false;
+		
+		//Parse Arguments
+		if (args.length < 1) port = 0; //Leave port as 0 for default
+	    else {
+	    	try {
+	    		port = Integer.parseInt(args[0]);
+	    	}
+	    	//If integer cannot be parsed terminate process.
+	    	catch (NumberFormatException ex) {
+	    		System.out.println("Passed arguments is not a number value... Process terminated.");
+	    		return;
+	    	}
+	    }
+		
+		//Mutate ports
+		unverifiedPort += port;
+		blockChainPort += port;
+		
+		//TODO: Read the data block
+		
+		//TODO: Verify digital signature
+		
+		//TODO: Notify other processes to start trying to resolve hash
+		
+		//TODO: Start randomizing blocks till winning hash is found by this process or others.
+		//Check if already solved
+		//Mark solved if solution is found
+		
+		//TODO: If process 0, write results to disk.
+	}
+}
+
+
 class DataBlock {
+	//Properties
 	String startHash;
 	String date;
 	String movie;
 	String genre;
 	String screeningDate;
 	int rating; //Rating out of 100
-	String randomSeed;
-	String endHash;
+	String randomSeed = "";
+	String endHash = "";
 	
+	//Getters and Setters
 	public String getStartHash() {return startHash;}
 	public void setStartHash(String startHash) {this.startHash = startHash;}
 	
@@ -53,38 +118,17 @@ class DataBlock {
 	}
 }
 
-public class BlockChain {
+//Listens for solutions from secondary processes.
+class SolutionListener {
+	//Properties
+	private int port;
+
+	//Getters and Setters
+	public int getPort() {return port;}
+	public void setPort(int port) {this.port = port;}
 	
-	static MessageDigest encoder;
-
-	public static void main (String [] args) {
-
-		//TODO: Instantiate variables
-		try {
-			encoder = MessageDigest.getInstance("SHA-256");
-		} 
-		//If encoding variable immediately close program after printing error code.
-		catch (NoSuchAlgorithmException exp) {
-			exp.printStackTrace();
-			return;
-		}
-		
-		//Run the process
-		new BlockChain().run(args);
-
-	}
-	
-	void run(String [] args) {
-		
-		//TODO: Parse Arguments
-		
-		//TODO: Read the data block
-		
-		//TODO: Notify other processes to start trying to resolve hash
-		
-		//TODO: Start randomizing blocks till winning hash is found by this process or others.
-	}
 }
+
 
 
 
